124466e82677d89b6ceddf555ffa52c8144cd49e
Ahmed <amamdouh247@gmail.com>
2021-04-22 02:34:06 +0200

    send update order request

--- src/components/CurrentOrdersComponent.vue
+++ src/components/CurrentOrdersComponent.vue
@@ -57,32 +57,43 @@ export default {
   methods: {
     updateStatus(order) {
       let formData = new FormData();
-      axios
-        .put(
-          `http://127.0.0.1:8000/api/order/${order}`,
-          {},
-          {
-            headers: {
-              Accept: "application/json",
-              Authorization: `Bearer ${this.accessToken}`,
-            },
-          }
-        )
-        .then((response) => {
-          console.log(response);
-          if (response.data.status == "success") {
-            for (let i = 0; i < this.currentOrders.length; i++) {
-              if (order.id === this.currentOrders[i]) {
-                this.currentOrders.splice(i, 1);
-              }
+      const selectedOrder = JSON.stringify(order);
+      console.log(selectedOrder);
+
+      fetch(`http://127.0.0.1:8000/api/order/1`, {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${this.accessToken}`,
+        },
+      })
+        // .then((response) => response.json())
+        // .then((json) => console.log(json));
+      // .then((response) => console.log(response.json()))
+
+      .then((data) => {
+        console.log(data);
+        console.log(data.message);
+        if (data.status === "Error") {
+          console.log(data.message);
+        } else {
+          for (let i = 0; i < this.currentOrders.length; i++) {
+            if (order.id === this.currentOrders[i].id) {
+              this.currentOrders.splice(i, 1);
+              break;
             }
-          } else {
-            console.log("could not update the order status");
+            
           }
-        });
+        }
+      });
+
+      
     },
     getAccessToken() {
-      this.accessToken = localStorage.getItem("user")["token"];
+      const user = JSON.parse(localStorage.getItem("user"));
+      this.accessToken = user["token"];
     },
   },
 
@@ -93,7 +104,6 @@ export default {
       .then((response) => response.json())
       .then((json) => {
         this.currentOrders = json.data;
-        console.log(this.currentOrders);
       });
   },
 };
