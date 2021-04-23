db71f613325f717049b63abb7577b053b4f04317
mohamedkaoud14 <mdkaoud14@gmail.com>
2021-04-23 23:06:43 +0200

    add changes

---
+++ src/components/AdminUserContainer.vue
@@ -0,0 +1,42 @@
+<template>
+<component :is="target"  @addUserClicked="mytarget"/>   
+
+</template>
+
+<script>
+ import AdminListUsers from "./AdminUserListComponent" 
+  import Register from "./RegisterComponent" 
+
+export default {
+    data (){
+
+        return {
+            target: "AdminListUsers"
+
+
+        }
+    } ,  
+    methods : {
+
+        mytarget(){
+
+            this.target = "Register"
+        }
+
+
+    } , 
+
+  components:{
+    
+         AdminListUsers , 
+         Register
+
+    
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
